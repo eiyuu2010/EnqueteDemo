c8161e424c0cc5c7fd09997b07212ab733efdca4
Hoang <hung@agentec.jp>
2016-02-23 16:48:51 +0900

    Add WindowForm

---
+++ WFormDemo/WFormDemo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFormDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.webBrowser1.ObjectForScripting = new ObjectScripting(this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            path = path + "\\test_ENQUETE\\index.html";
+            
+            Uri uri = new Uri(path);
+            webBrowser1.Navigate(uri);
+        }
+    }
+}
